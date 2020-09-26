a6a7e8d26711f85e6ee0a5a35c219d17ca1cd5ee
Nikita Prudnikov <71658211+PrudnikovUNN@users.noreply.github.com>
2020-09-26 10:46:39 +0300

    Add files via upload

--- Task 0.cpp
+++ Task 0.cpp
@@ -1,31 +1,36 @@
-﻿
-Microsoft Visual Studio Solution File, Format Version 12.00
-# Visual Studio Version 16
-VisualStudioVersion = 16.0.30413.136
-MinimumVisualStudioVersion = 10.0.40219.1
-Project("{8BC9CEB8-8B4A-11D0-8D11-00A0C91BC942}") = "Task 0", "Task 0\Task 0.vcxproj", "{AAA3951C-0DD1-4958-A3DA-8FD2DC08A7C8}"
-EndProject
-Global
-	GlobalSection(SolutionConfigurationPlatforms) = preSolution
-		Debug|x64 = Debug|x64
-		Debug|x86 = Debug|x86
-		Release|x64 = Release|x64
-		Release|x86 = Release|x86
-	EndGlobalSection
-	GlobalSection(ProjectConfigurationPlatforms) = postSolution
-		{AAA3951C-0DD1-4958-A3DA-8FD2DC08A7C8}.Debug|x64.ActiveCfg = Debug|x64
-		{AAA3951C-0DD1-4958-A3DA-8FD2DC08A7C8}.Debug|x64.Build.0 = Debug|x64
-		{AAA3951C-0DD1-4958-A3DA-8FD2DC08A7C8}.Debug|x86.ActiveCfg = Debug|Win32
-		{AAA3951C-0DD1-4958-A3DA-8FD2DC08A7C8}.Debug|x86.Build.0 = Debug|Win32
-		{AAA3951C-0DD1-4958-A3DA-8FD2DC08A7C8}.Release|x64.ActiveCfg = Release|x64
-		{AAA3951C-0DD1-4958-A3DA-8FD2DC08A7C8}.Release|x64.Build.0 = Release|x64
-		{AAA3951C-0DD1-4958-A3DA-8FD2DC08A7C8}.Release|x86.ActiveCfg = Release|Win32
-		{AAA3951C-0DD1-4958-A3DA-8FD2DC08A7C8}.Release|x86.Build.0 = Release|Win32
-	EndGlobalSection
-	GlobalSection(SolutionProperties) = preSolution
-		HideSolutionNode = FALSE
-	EndGlobalSection
-	GlobalSection(ExtensibilityGlobals) = postSolution
-		SolutionGuid = {D4388D14-01D7-4BED-BFD5-25139E59AF42}
-	EndGlobalSection
-EndGlobal
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <stdio.h>
+#include<math.h>
+
+
+using namespace std;
+
+int main()
+{
+    double height, Width, depth, Thickness_zs, Thickness_bvns, Thickness_dv; // (th_ - толщина ) zs - задней стенки,bvns - боковой, верхней, нижней (стенки,крышки), dv - двери,полки
+    double Density_Dsp, Density_Dvp, Density_Wood;//(Density_ плотность) Dsp - ДСП, Dvp - ДВП,  Wood - дерева
+    double weight; //масса
+
+    Thickness_zs = 0.005;
+    Thickness_bvns = 0.015;
+    Thickness_dv = 0.01;
+
+    scanf("%f%f%f", &height, &depth, &Width);
+    scanf("%f%f%f", &Density_Dsp, &Density_Dvp, &Density_Wood);//-------------ЭТО 1-АЯ ЗАДАЧА, А НЕ 0) Вечером обе скину в норм виде
+
+    height /= 100; //переводим в систему СИ
+    depth /= 100;
+    Width /= 100;
+
+    weight = (height * Width * Thickness_zs * Density_Dvp)
+        + (height * depth * Thickness_bvns * Density_Dsp * 2)
+        + (Width * depth * Thickness_bvns * Density_Dsp * 2)
+        + (height * Width * Thickness_dv * Density_Wood)
+        + (4 * depth * Width * Density_Dsp * Thickness_dv);
+
+    printf("%d", &height);
+    system("pause");
+    return 0;
+}
+
